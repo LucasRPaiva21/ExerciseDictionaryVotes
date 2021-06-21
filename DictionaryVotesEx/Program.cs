5faa348af050054be5ff07b8483fbb48cc8c38dc
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-21 01:01:35 -0300

    Versão Final

--- DictionaryVotesEx/Program.cs
+++ DictionaryVotesEx/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace DictionaryVotesEx
 {
@@ -6,7 +8,40 @@ namespace DictionaryVotesEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> candidateVotes = new Dictionary<string, int>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+                        if (candidateVotes.ContainsKey(name))
+                        {
+                            candidateVotes[name] += votes;
+                        }
+                        else
+                        {
+                            candidateVotes[name] = votes;
+                        }
+                    }
+
+                    foreach (KeyValuePair<string, int> item in candidateVotes)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
